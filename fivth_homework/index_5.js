d7ec338264b6b8ec28bd5335e1ae94b85841623e
SerzhantAnton <64481403+SerzhantAnton@users.noreply.github.com>
2021-04-21 15:47:46 +0300

    Add files via upload

---
+++ fivth_homework/index_5.js
@@ -0,0 +1,31 @@
+// 6
+function buttonClick_1() {
+    let inp1 = document.getElementById('input_1');
+    inp1.value = 'Ой, я поменял свой текст!'
+}
+
+ // 7
+ function buttonClick_2() {
+    let inp2 = document.getElementById('input_2');
+    inp2.src = "img/2.jpg"
+}
+
+// 9
+function buttonClick_3() { 
+    let inp3 = document.getElementById('input_3');
+    inp3.value = "ой, я поменял свой текст!"
+}
+
+
+// 10
+function buttonClick_4() {
+    let inp4 = document.getElementById('input_4');
+    let inp5 = document.getElementById('input_5');
+    inp4.value = "ой, я поменял свой текст!";
+    inp5 = inp4.value;
+    inp5.value = "Еще раз я поменял свой текст!";
+    
+}
+
+
+
